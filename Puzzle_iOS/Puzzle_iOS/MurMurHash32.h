a96870f91a68c2209df08361b5214b7a6ac443c2
LawrenceHan <yangfei6565@163.com>
2018-09-21 15:59:28 +0800

    test 25

--- Puzzle_iOS/Puzzle_iOS/MurMurHash32.h
+++ Puzzle_iOS/Puzzle_iOS/MurMurHash32.h
@@ -3,5 +3,3 @@
 
 int32_t murMurHash32(void *bytes, int length);
 int32_t murMurHashString32(const char *s);
-NSString *postboxTransformedString(CFStringRef string, bool replaceWithTransliteratedVersion, bool appendTransliteratedVersion);
-
